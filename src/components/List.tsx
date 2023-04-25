446d364566fffafc3776e9d50c15d33a25165905
Drakonz <124328116+DrakonZZZ@users.noreply.github.com>
2023-04-25 20:03:31 +0530

    bootstrap with react

---
+++ src/components/List.tsx
@@ -0,0 +1,16 @@
+export default function List() {
+  return (
+    <>
+      <h1>Director List</h1>
+      <ul className="list-group">
+        <li className="list-group-item active" aria-current="true">
+          Top 5
+        </li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
+}
